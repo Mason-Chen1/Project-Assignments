9dd28aaf8b061908c2771d8e9a6169c157155299
Mason-Chen1 <mason@hackthehood.org>
2021-06-16 09:37:07 -0700

    adding python file

---
+++ Videos/Program-Assignments/helloworld.py
@@ -0,0 +1,5 @@
+print("Hello World!!")
+
+var1 = 6
+var2 = 13
+print(var1 + var2)
---
+++ Videos/Program-Assignments/stephcurry.py
@@ -0,0 +1,13 @@
+name = "Curry"
+age = 33
+salary = 40.23
+playsBasketball = True
+jerseyNumber = "30"
+print(name, age, salary, playsBasketball, jerseyNumber)
+
+carName = "Ford"
+print(type(carName))
+x = 50
+print(type(x))
+first_name = "Mason"
+print(type(first_name))
