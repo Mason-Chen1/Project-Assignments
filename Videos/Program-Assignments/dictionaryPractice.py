562fbec6efff8b397557e3ab81593ab2dcb67d5d
Mason-Chen1 <mason@hackthehood.org>
2021-06-17 09:13:54 -0700

    Dictionary Practice

---
+++ Videos/Program-Assignments/dictionaryPractice.py
@@ -0,0 +1,17 @@
+foodPrice = {"Chicken": 1.59, "Beef": 1.99, "Cheese": 1.00, "Milk": 2.50}
+nbaPlayers = {"Lebron James": 23, "Kevin Durant": 35, "Stephen Curry": 30, "Damian Lillard": 0}
+familyAge = {"Dad": 50, "Mom": 47, "Oldest Brother": 20, "Middle Brother": 18, "Me": 16} 
+shoeQuantity = {"Jordon 13": 1, "Yeezy": 8, "Foamposite": 10, "Air Max": 5, "SB Dunk": 20}
+
+chickenPrice = foodPrice["Chicken"]
+beefPrice = foodPrice["Beef"]
+cheesePrice = foodPrice["Cheese"]
+milkPrice = foodPrice["Milk"]
+print(chickenPrice)
+print(beefPrice)
+print(cheesePrice)
+print(milkPrice)
+
+nbaPlayers["Lebron James"] = 6
+jerseyNumber = nbaPlayers["Lebron James"]
+print(jerseyNumber)
