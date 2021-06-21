84b41f81686f04b6fc5e64b0f968d4358a4124d3
unknown <mason@hackthehood.com>
2021-06-21 10:07:23 -0700

    Lab 4

---
+++ Videos/Program-Assignments/Lab4.py
@@ -0,0 +1,33 @@
+foodPrice = {"Chicken": 1.59, "Beef": 1.99, "Cheese": 1.00, "Milk": 2.50}
+
+def sumPrice(price1, price2):
+    sum = (price1 + price2)
+    print("The total price of Chicken and Beef is " + str(sum))
+
+print(sumPrice(foodPrice.get("Chicken"), foodPrice.get("Beef")))
+
+def differencePrice(price1, price2):
+    difference = (price1 - price2)
+    print("The difference of Beef and Chicken is " + str(difference))
+
+print(differencePrice(foodPrice.get("Beef"), foodPrice.get("Chicken")))
+
+
+shoeQuantity = {"Jordon 13": 1, "Yeezy": 8, "Foamposite": 10, "Air Max": 5, "SB Dunk": 20}
+
+def restock(item1, item2):
+    new = (item1 * item2)
+    print("There are now " + str(new) + " pairs of Yeezy's")
+
+print(restock(shoeQuantity.get("Yeezy"), 3))
+
+def clearanceSale(item1, item2):
+    amount = (item1 / item2)
+    print("We only have " + str(amount) + " pairs of SB Dunks left")
+
+print(clearanceSale(shoeQuantity.get("SB Dunk"), 5))
+
+#Sorting a dictionary by values
+sort = sorted(foodPrice.items(), key=lambda x: x[1])
+for i in sort:
+    print(i[0], i[1])
--- Videos/Program-Assignments/loopsPractice.py
+++ Videos/Program-Assignments/loopsPractice.py
@@ -10,20 +10,38 @@ for i in range(0,100,5)
     print(i)
 '''
 
+def fibonacciFinder (max):
+    i = 0
+    j = 1
+    k = 0
+    while i < max:
+        print(k)
+        k = i + j
+        i = j
+        j = k
+    return("Fibonacci!!")
+
+def primeFinder (max):
+    for i in range(2,max):
+        for j in range (2,i):
+            if i % j == 0:
+                break
+        else:
+            print(i)
+    return(max)
+
+def triangleArea(base, height):
+    area = base*height/2
+    return area
+triangleArea(3,6)
+
+n = 5
+m = 5
 i = 0
-j = 1
-k = 0
-while i < 100:
-    print(k)
-    k = i + j
-    i = j
-    j = k
+areaList = [0]*n*m
+for b in range(0,n):
+    for h in range(0,m):
+        areaList[i] = triangleArea(b,h)
+        i = i + 1
 
-for i in range(1,1000):
-    for j in range (2,i):
-        if i % j == 0:
-            break
-    else:
-        print(i)
-
-
+print(areaList)
